349be89103629fbe34b413b466174f8881ea922b
Zer0-M <msj2340@gmail.com>
2017-11-22 15:28:17 -0500

    created java file for the ReversedCharSequence and added a constructor and an incomplete method

---
+++ 04ReversedCharSeq/ReversedCharSeq.java
@@ -0,0 +1,16 @@
+public class ReversedCharSeq implements CharSequence{
+    private String seq;
+    public ReversedCharSeq(String str){
+	String reversed=new String(str.length);
+	for(int i=str.length();i>=0;i--){
+	    s+=str.substring(i,i-1);
+	}
+	    
+    }
+    public int charAt(int index){
+	for(int i=0;i<str.length();i++){
+	    
+	}
+    }
+    
+}
