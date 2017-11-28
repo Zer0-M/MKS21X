61383d8a74c0c5fe529d2be52a234f0b1d043287
Zer0-M <msj2340@gmail.com>
2017-11-28 15:14:08 -0500

    created Book.java; added constructors and getters and setters

---
+++ 05Library/Book.java
@@ -0,0 +1,38 @@
+public class abstract Book{
+    private String author;
+    private String title;
+    private String ISBN;
+    public Book(){
+    }
+    public Book(String writer,String name,String isbn){
+	author=writer;
+	title=name
+	ISBN=isbn
+    }
+    public String toString(){
+    }
+    public String getAuthor(){
+	return author;
+    }
+    public String getTitle(){
+	return title;
+    }
+    public String getISBN(){
+	return ISBN;
+    }
+    public boolean setAuthor(String writer){
+	author=writer;
+	return author==writer;
+    }
+    public boolean setTitle(String name){
+	title=name;
+	return title==name;
+    }
+    public boolean setISBN(String isbn){
+	ISBN=isbn;
+	return ISBN==isbn;
+    }
+    
+    
+    
+}
