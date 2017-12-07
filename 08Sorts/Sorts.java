a9097ad7b970b77be8d575a907f96614f9854737
Mohammed Shafwat Jamil <msj2340@gmail.com>
2017-12-06 21:37:18 -0500

    added and tested sort methods

---
+++ 08Sorts/Sorts.java
@@ -0,0 +1,62 @@
+import java.util.*;
+public class Sorts{
+  /**EDIT THIS METHOD TO MATCH YOUR NAME
+   */
+  public static String name(){
+    return "10.Jamil.Mohammed";
+  }
+  /**isSorted method created by Mr. K
+   */
+  public static boolean isSorted(int[]ary){
+    for(int i = 0; i < ary.length - 1 ; i++){
+      if(ary[i] > ary[i+1]){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**Selection sort of an int array.
+   *Upon completion, the elements of the array will be in increasing order.
+   *@param data  the elements to be sorted.
+   */
+  public static void selectionSort(int[] data){
+    int indexOfSmallest=0;
+    int ind=0;
+    while(!isSorted(data)){
+      int smallest=data[ind];
+      for(int i=ind;i<data.length;i++){
+        if(data[i]<smallest){
+          smallest=data[i];
+          indexOfSmallest=i;
+        }
+      }
+      int temp=data[ind];
+      data[ind]=smallest;
+      data[indexOfSmallest]=temp;
+      ind++;
+    }
+  }
+  public static void insertionSort(int[] data){
+    while(!isSorted(data)){
+      for(int i=0;i<data.length;i++){
+        int temp=data[i];
+        if(i!=0&&data[i-1]>data[i]){
+          data[i]=data[i-1];
+          data[i-1]=temp;
+        }
+      }
+    }
+  }
+  public static void main(String[] arg){
+    int[] ary=new int[100];
+    for(int i=0;i<ary.length;i++){
+      ary[i]=(int)(Math.random()*100);
+    }
+    System.out.println(Arrays.toString(ary));
+    selectionSort(ary);
+    //insertionSort(ary);
+    System.out.println(Arrays.toString(ary));
+  }
+
+}
