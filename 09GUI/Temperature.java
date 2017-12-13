9be6559aaae1cebbcaddc32920e4ec7b3d14a7ba
Zer0-M <msj2340@gmail.com>
2017-12-13 10:28:31 -0500

    made 09GUI and placed previously created java files in this directory

---
+++ 09GUI/Temperature.java
@@ -0,0 +1,9 @@
+public class Temperature{
+    public static double CtoF(double t){
+	return (9.0/5.0)*t+32.0;
+    }
+    public static double FtoC(double t){
+	return (t-32)*(5.0/9.0);
+    }
+
+}
---
+++ 09GUI/Window.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+public class Window2 extends JFrame implements ActionListener {
+    private Container pane;
+    private JCheckBox CtoF;
+    private JCheckBox FtoC;
+    private JButton convert;
+    private JTextField t;
+ 
+  //CONSTRUCTOR SETS EVERYTHING UP
+  public void actionPerformed(ActionEvent e) { 
+      String s =e.getActionCommand();
+      System.out.println(s);
+      //if(CtoF.isSelected()){
+      //	  s+=Temperature.CtoF(Integer.parseInt(t.getText()));
+      //}
+
+      
+  } 
+  public Window() {
+     this.setTitle("My first GUI");
+     this.setSize(600,400);
+     this.setLocation(100,100);
+     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    
+     pane = this.getContentPane();
+     pane.setLayout(new FlowLayout());  //NOTE!! THIS CAN BE CHANGED (see below)
+    
+     FtoC = new JCheckBox("Farenheit to Celsius");
+     CtoF = new JCheckBox("Celsius to Farenheit");
+     convert= new JButton("Convert");
+     t = new JTextField(12);
+     convert.addActionListener(this);
+     pane.add(FtoC);
+     pane.add(CtoF);
+     pane.add(t);
+     pane.add(convert);
+ }
+
+  //MAIN JUST INSTANTIATES + MAKE VISIBLE
+  public static void main(String[] args) {
+     Window g = new Window();
+     g.setVisible(true);
+  }
+}
